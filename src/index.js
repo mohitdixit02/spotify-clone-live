5fdad1bb5f787a927b1c67fc854065ba6c796ebe
Mohit Sharma <m_sharma@me.iitr.ac.in>
2023-01-28 02:12:00 +0530

    Play Image width set

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/spotify-clone-live/'>
+    <BrowserRouter 
+    basename='/spotify-clone-live/'
+    >
     <App />
     </BrowserRouter>
   </React.StrictMode>
