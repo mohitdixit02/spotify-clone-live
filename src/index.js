cae6ce66e2eee3eb3fb7707671428a70b720bfcb
Mohit Sharma <m_sharma@me.iitr.ac.in>
2023-01-27 23:09:48 +0530

    url info updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { frontend_url } from './components/service component/url_info';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={frontend_url}>
     <App />
     </BrowserRouter>
   </React.StrictMode>
