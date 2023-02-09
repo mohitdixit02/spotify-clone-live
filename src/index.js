0b60431352df3afe72220bef9cab47b5cec89259
Mohit Sharma <m_sharma@me.iitr.ac.in>
2023-02-09 19:56:10 +0530

    url updated

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter 
-    // basename='/spotify-clone-live/'
+    basename='/spotify-clone-live/'
     >
     <App />
     </BrowserRouter>
