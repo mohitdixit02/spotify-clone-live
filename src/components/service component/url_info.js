d22cb6001b997099d78f74c5b51ecb6589054d17
Mohit Sharma <m_sharma@me.iitr.ac.in>
2023-02-09 12:04:35 +0530

    checking main height

--- src/components/service component/url_info.js
+++ src/components/service component/url_info.js
@@ -1,5 +1,5 @@
-// const backend_url='http://127.0.0.1:8000'
-const backend_url='https://mohitvsht02.pythonanywhere.com'
+const backend_url='http://127.0.0.1:8000'
+// const backend_url='https://mohitvsht02.pythonanywhere.com'
 // const frontend_url='http://localhost:3000'
 const frontend_url='/spotify-clone-live'
 
